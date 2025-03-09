17bfc2875dc61d1f63f173885c0af1362b58076f
Jeremy Kaplan <code@jdkaplan.dev>
2025-03-08 19:30:02 -0500

    Dev setup

---
+++ README.md
@@ -0,0 +1,3 @@
+These are my solutions to the internationalization puzzles at [i18n-puzzles.com](https://i18n-puzzles.com/).
+
+Out of the languages I've written production code in, JS been the least reassuring ecosystem in terms of feeling like I've ever gotten these topics "right". So I'm doing these in TypeScript to force as many bugs and fixes as I can!
---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+//@ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  // I don't entirely know _why_ this is considered an unsafe access, but this
+  // config is definitely going to be there.
+  //
+  //eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.mjs"],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+);
